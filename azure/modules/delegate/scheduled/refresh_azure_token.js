1085e3bd00a5fff8879ff541c10e36dacfc0f4fa
isoubelet <isoubelet@bluelone.com>
2024-06-04 19:44:38 +0200

    Fixed erroneous refresh token script.

--- azure/modules/delegate/scheduled/refresh_azure_token.js
+++ azure/modules/delegate/scheduled/refresh_azure_token.js
@@ -1,13 +1,6 @@
 import { generateAzureToken } from "../platform/api/TokenGenerator";
 
 var deltaMinutes = (global.get("azure_api_client_token_exp") - helper.dt().nowSeconds()) / 60;
-
-print(global.get("azure_api_client_token_exp"));
-print(helper.dt().nowSeconds());
 print("Delta minutes " + deltaMinutes);
 
-try {    
-    if (deltaMinutes <= 8) generateAzureToken();
-} catch (e) {
-    initResult.error(e.message);
-}
+if (deltaMinutes <= 8) generateAzureToken();
